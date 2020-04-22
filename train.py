83c389a26b51b09fb4516d2025926d09d9f5c94a
daniel.silver <daniel@silverengineered.com>
2020-04-22 16:30:28 -0400

    s

--- train.py
+++ train.py
@@ -28,10 +28,6 @@ if __name__ == '__main__':
 
             total_iters += opt.batch_size
             epoch_iter += opt.batch_size
-            macs, params = profile(model, data)
-            print(macs)
-            print(params)
-            exit()
             model.set_input(data)         # unpack data from dataset and apply preprocessing
             model.optimize_parameters()   # calculate loss functions, get gradients, update network weights
 
