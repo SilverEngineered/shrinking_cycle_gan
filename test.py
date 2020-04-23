b1d61359d79ef4ee30840551db4573f08da0e431
daniel.silver <daniel@silverengineered.com>
2020-04-22 21:40:30 -0400

    teacher

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ from util import html
 
 if __name__ == '__main__':
     opt = TestOptions().parse()  # get test options
+    print(opt)
+    exit()
     # hard-code some parameters for test
     opt.num_threads = 0   # test code only supports num_threads = 1
     opt.batch_size = 1    # test code only supports batch_size = 1
