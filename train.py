92975979f893f469856db0fdaf419fedf9791f5a
daniel.silver <daniel@silverengineered.com>
2020-04-22 22:36:31 -0400

    s

--- train.py
+++ train.py
@@ -7,14 +7,13 @@ from models.cycle_gan_with_distillation import CycleGANModelWithDistillation
 from util.visualizer import Visualizer
 from thop import profile
 from tqdm import tqdm
-
+from argparse import Namespace
 if __name__ == '__main__':
     opt = TrainOptions().parse()   # get training options
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     dataset_size = len(dataset)    # get the number of images in the dataset.
     print('The number of training images = %d' % dataset_size)
-    opt2 = opt
-    print(type(opt))
+    opt2 =  Namespace(**vars(opt))
     opt2.name = 'monet2photo_pretrained'
     opt2.isTrain = False
     print(opt)
