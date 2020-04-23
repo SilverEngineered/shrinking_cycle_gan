561d7c68457d81418c57c5b8e65b314a163e2c43
daniel.silver <daniel@silverengineered.com>
2020-04-23 16:57:47 -0400

    train

--- train.py
+++ train.py
@@ -65,7 +65,7 @@ if __name__ == '__main__':
                 img_path = model.get_image_paths()  # get image paths
                 #if i % 1 == 0:  # save images to an HTML file
                     #print('processing (%04d)-th image... %s' % (i, img_path))
-                save_images(webpage, visuals, img_path, aspect_ratio=opt2.aspect_ratio, width=opt2.display_winsize)
+                save_images(webpage, visuals, img_path)
 
             if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
                 losses = model.get_current_losses()
