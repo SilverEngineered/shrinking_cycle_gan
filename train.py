6e1ccb9c01de989e820784f2e9ff0b4bb635aa58
daniel.silver <daniel@silverengineered.com>
2020-04-22 22:41:51 -0400

    s

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     dataset_size = len(dataset)    # get the number of images in the dataset.
     print('The number of training images = %d' % dataset_size)
-    opt2 =  Namespace(**vars(opt))
+    opt2 = Namespace(**vars(opt))
     opt2.name = 'monet2photo_pretrained'
     opt2.isTrain = False
     teacher = CycleGANModel(opt2)
