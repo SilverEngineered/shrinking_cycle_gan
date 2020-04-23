6e3c50733373575b83b5cbced031a3c7c571529c
daniel.silver <daniel@silverengineered.com>
2020-04-22 22:09:00 -0400

    circumventing setup

--- models/base_model.py
+++ models/base_model.py
@@ -41,6 +41,7 @@ class BaseModel(ABC):
         self.visual_names = []
         self.optimizers = []
         self.image_paths = []
+        self.isTeacher = False
         self.metric = 0  # used for learning rate policy 'plateau'
 
     @staticmethod
@@ -81,7 +82,7 @@ class BaseModel(ABC):
         Parameters:
             opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
         """
-        if self.isTrain or self.isTeacher is not None:
+        if self.isTrain or self.isTeacher:
             self.schedulers = [networks.get_scheduler(optimizer, opt) for optimizer in self.optimizers]
         elif not self.isTrain or opt.continue_train:
             load_suffix = 'iter_%d' % opt.load_iter if opt.load_iter > 0 else opt.epoch
