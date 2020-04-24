e76c02a3fd67ccd774af779a79414a2f503c989d
daniel.silver <daniel@silverengineered.com>
2020-04-23 21:37:29 -0400

    stuff

--- models/cycle_gan_with_distillation.py
+++ models/cycle_gan_with_distillation.py
@@ -78,10 +78,11 @@ class CycleGANModelWithDistillation(BaseModel):
 
         The option 'direction' can be used to swap domain A and domain B.
         """
-        AtoB = self.opt.direction == 'AtoB'
-        self.real_A = input['A' if AtoB else 'B'].to(self.device)
-        self.real_B = input['B' if AtoB else 'A'].to(self.device)
-        self.image_paths = input['A_paths' if AtoB else 'B_paths']
+        if test is False:
+            AtoB = self.opt.direction == 'AtoB'
+            self.real_A = input['A' if AtoB else 'B'].to(self.device)
+            self.real_B = input['B' if AtoB else 'A'].to(self.device)
+            self.image_paths = input['A_paths' if AtoB else 'B_paths']
         if test:
             self.real = input['A'].to(self.device)
             self.image_paths = input['A_paths']
