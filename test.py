f70587c3fd94279471c839c84dcabe3720a575c7
daniel.silver <daniel@silverengineered.com>
2020-04-23 21:13:32 -0400

    stuff

--- test.py
+++ test.py
@@ -43,6 +43,8 @@ if __name__ == '__main__':
     for i, data in enumerate(dataset):
         if i >= opt.num_test:  # only apply our model to opt.num_test images.
             break
+        print(data)
+        print(data['B'])
         model.set_input(data)  # unpack data from data loader
         model.test()           # run inference
         visuals = model.get_current_visuals()  # get image results
