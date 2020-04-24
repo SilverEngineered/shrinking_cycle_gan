9b2589b761b09e8118298db37a5c43aa2f7a171c
daniel.silver <daniel@silverengineered.com>
2020-04-23 20:17:58 -0400

    ready to deploy

--- test.py
+++ test.py
@@ -4,8 +4,9 @@ from data import create_dataset
 from models import create_model
 from util.visualizer import save_images
 from util import html
-
-
+from argparse import Namespace
+from models.cycle_gan_with_distillation import  CycleGANModelWithDistillation
+from models.cycle_gan_model import CycleGANModel
 if __name__ == '__main__':
     opt = TestOptions().parse()  # get test options
     # hard-code some parameters for test
@@ -15,7 +16,18 @@ if __name__ == '__main__':
     opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
     opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
-    model = create_model(opt)      # create a model given opt.model and other options
+
+    opt2 = Namespace(**vars(opt))
+    opt2.name = 'monet2photo_pretrained'
+    opt2.isTrain = False
+    teacher = CycleGANModel(opt2)
+    teacher.isTeacher = True
+    teacher.setup(opt2)  # regular setup: load and print networks; create schedulers
+    opt.netG = 'resnet_3blocks'
+    opt.results_dir = 'results'
+
+
+    model = CycleGANModelWithDistillation(opt, teacher)      # create a model given opt.model and other options
     model.setup(opt)              # regular setup: load and print networks; create schedulers
     # create a website
     web_dir = os.path.join(opt.results_dir, opt.name, '{}_{}'.format(opt.phase, opt.epoch))  # define the website directory
